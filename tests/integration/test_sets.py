bd0f48ab1ac042e47a27a0beffa70279167b0ec0
George Leslie-Waksman <waksman@gmail.com>
2023-03-28 00:10:24 -0700

    Add tests for sets

---
+++ tests/integration/test_sets.py
@@ -0,0 +1,32 @@
+from typing import TYPE_CHECKING
+from uuid import UUID
+
+from pscryfall import sets
+
+if TYPE_CHECKING:
+    from aiohttp import ClientSession
+
+
+async def test_all_sets(client_session: "ClientSession") -> None:
+    result = await sets.all_sets(client_session)
+    assert result.data
+
+
+async def test_code(client_session: "ClientSession") -> None:
+    result = await sets.code(client_session, "ice")
+    assert result.code == "ice"
+    assert result.name == "Ice Age"
+
+
+async def test_tcgplayer_id(client_session: "ClientSession") -> None:
+    result = await sets.tcgplayer_id(client_session, 1857)
+    assert result.code == "aer"
+    assert result.name == "Aether Revolt"
+
+
+async def test_get(client_session: "ClientSession") -> None:
+    result = await sets.get(
+        client_session, UUID("a4a0db50-8826-4e73-833c-3fd934375f96")
+    )
+    assert result.code == "aer"
+    assert result.name == "Aether Revolt"
